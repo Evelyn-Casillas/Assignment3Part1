b3400af434c2b21631ed5594827bc183ef1fad37
Evelyn-Casillas <evelyn.casillas@outlook.com>
2025-02-22 03:20:14 -0800

    Save all changes

--- app.py
+++ app.py
@@ -1,28 +1,46 @@
 from flask import Flask, render_template, request
 import numpy as np
-import pickle
 import joblib
+
 app = Flask(__name__)
-filename = 'file_iris.pkl'
-#model = pickle.load(open(filename, 'rb'))
+filename = 'LogisticModel.pkl'
+
+# Load the model (adjust the model to accept 11 features)
 model = joblib.load(filename)
-#model = joblib.load(filename)
+
+
 @app.route('/')
-def index(): 
+def index():
     return render_template('index.html')
+
+
 @app.route('/predict', methods=['POST'])
 def predict():
-    Sepal_Length = request.form['sepal_length']
-    Sepal_Width = request.form['sepal_width']
-    Petak_Length = request.form['petal_length']
-    Petal_Width = request.form['petal_width']
+    # Collect the 11 features from the form
+    feature1 = float(request.form[
+                         'feature1'])  # Example for first feature (you'll need to change these names based on the features in your model)
+    feature2 = float(request.form['feature2'])
+    feature3 = float(request.form['feature3'])
+    feature4 = float(request.form['feature4'])
+    feature5 = float(request.form['feature5'])
+    feature6 = float(request.form['feature6'])
+    feature7 = float(request.form['feature7'])
+    feature8 = float(request.form['feature8'])
+    feature9 = float(request.form['feature9'])
+    feature10 = float(request.form['feature10'])
+    feature11 = float(request.form['feature11'])
 
-    
-      
-    pred = model.predict(np.array([[Sepal_Length, Sepal_Width, Petak_Length, Petal_Width ]]))
-    print(pred)
+    # Prepare the features for prediction
+    features = np.array([[feature1, feature2, feature3, feature4, feature5, feature6, feature7, feature8, feature9,
+                          feature10, feature11]])
+
+    # Predict using the model
+    pred = model.predict(features)
+
     return render_template('index.html', predict=str(pred))
 
 
 if __name__ == '__main__':
-    app.run
+    # Bind the Flask app to all available IPs (0.0.0.0) so it's accessible outside the container
+    app.run(host="0.0.0.0", port=5000, debug=True)
+
